ac37fc2fa3128fcfce42814593eb7fcc726abb77
calebjoshuapaul <calebjoshuapaul@gmail.com>
2022-11-02 10:36:25 +0530

    Feature: Add marker on double click and dragable marker

--- components/MapComponent.tsx
+++ components/MapComponent.tsx
@@ -1,30 +1,111 @@
-import React from "react";
-import Map from "react-map-gl";
+import React, { useContext, useState } from "react";
+import Head from "next/head";
+import Map, { Marker } from "react-map-gl";
+import { BookmarksContext } from "../context/BookmarksContext";
 
 interface MapComponentProps {
   className?: string;
 }
 
-const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_TOKEN || "";
+interface Bookmark {
+  name: string;
+  Lat: number;
+  Lng: number;
+}
 
+const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 /*
 This component is responsible for rendering the map. This component has to show the map and 
 markers bookmarked by users.
 */
 const MapComponent = (props: MapComponentProps): JSX.Element => {
+  const { bookmarks, setBookmarks } = useContext(BookmarksContext);
+
+  const [markerLat, setMarkerLat] = useState(18.37148);
+  const [markerLng, setMarkerLng] = useState(78.07017);
+
+  const handleAddBookmark = () => {
+    const bookmarkToAdd = prompt("Give your bookmark a name");
+
+    if (bookmarkToAdd) {
+      setBookmarks(() => [
+        ...bookmarks,
+        {
+          name: bookmarkToAdd,
+          Lat: markerLat,
+          Lng: markerLng,
+        },
+      ]);
+    } else alert("Please provide a name");
+  };
+
+  const handleDblClick = (evt: any) => {
+    const bookmarkToAdd = prompt("Give your bookmark a name");
+
+    if (bookmarkToAdd) {
+      setBookmarks(() => [
+        ...bookmarks,
+        {
+          name: bookmarkToAdd,
+          Lat: evt.lngLat.lat,
+          Lng: evt.lngLat.lng,
+        },
+      ]);
+    } else alert("Please provide a name");
+  };
+
   return (
     <div className={props.className}>
+      <Head>
+        <title>Test</title>
+        <link
+          href="https://api.mapbox.com/mapbox-gl-js/v2.10.0/mapbox-gl.css"
+          rel="stylesheet"
+          key="test"
+        />
+      </Head>
       <h3>Map</h3>
       <Map
         initialViewState={{
-          latitude: 37.7577,
-          longitude: -122.4376,
-          zoom: 8,
+          latitude: 18.37148,
+          longitude: 78.07017,
+          zoom: 4,
         }}
         mapboxAccessToken={MAPBOX_TOKEN}
         style={{ width: 600, height: 400 }}
         mapStyle="mapbox://styles/mapbox/streets-v9"
-      />
+        doubleClickZoom={false}
+        onDblClick={(evt) => handleDblClick(evt)}
+      >
+        <Marker
+          longitude={markerLng}
+          latitude={markerLat}
+          anchor="bottom"
+          draggable={true}
+          onDrag={(evt: any) => {
+            setMarkerLat(evt.lngLat.lat.toFixed(5));
+            setMarkerLng(evt.lngLat.lng.toFixed(5));
+          }}
+        />
+        {bookmarks.map((bookmark: Bookmark, index: any) => {
+          return (
+            <Marker
+              key={index}
+              longitude={bookmark.Lng}
+              latitude={bookmark.Lat}
+              anchor="bottom"
+              color="#ff0000"
+            />
+          );
+        })}
+      </Map>
+
+      <div>
+        <p>
+          Latitude: {markerLat} Longitude: {markerLng}
+        </p>
+        <button onClick={handleAddBookmark}>Add to bookmarks</button>
+      </div>
     </div>
   );
 };
